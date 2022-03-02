d7f6aecb586f5a7e5f5af8dfba11c3309d8d2f91
Luis Soto <sotoluis90@gmail.com>
2022-03-02 11:21:28 -0500

    Added initial bot code

---
+++ src/main/java/net/zoomiti/countmein/Main.java
@@ -0,0 +1,21 @@
+package net.zoomiti.countmein;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        String token = args[0];
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+
+    }
+}
